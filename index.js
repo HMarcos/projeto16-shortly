1f322a43bc8de9c9fd643da92dc62d979c0245d7
Marcos Henrique <marcosmarcone48@gmail.com>
2022-06-09 01:13:56 -0300

    Initial Configuration

---
+++ index.js
@@ -0,0 +1,19 @@
+import express, {json} from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import {info} from "./logging/logging.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(info(`The application is running on the PORT ${PORT}...`));
+});
---
+++ logging/logging.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+export const info = chalk.green.bold;
+export const debug = chalk.blue;
+export const error = chalk.red.bold;
+export const warning = chalk.yellow;
